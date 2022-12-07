58a683374b8158ec80841ad54f34f77a8f56a670
Sashko Milushev <milushev.sashko@gmail.com>
2022-12-07 18:56:36 +0200

    Implement edit and details photo views and template.

--- fishbook/photos/urls.py
+++ fishbook/photos/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 
-from fishbook.photos.views import add_photo
+from fishbook.photos.views import add_photo, details_photo, edit_photo
 
 urlpatterns = (
     path('add/', add_photo, name='add photo'),
+    path('<int:pk>/', include([
+        path('', details_photo, name='details photo'),
+        path('edit/', edit_photo, name='edit photo'),
+    ]))
+
 )
--- fishbook/photos/views.py
+++ fishbook/photos/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.views import generic as views
 
 from fishbook.photos.forms import PhotoCreateForm
+from fishbook.photos.models import Photo
 
 
 @login_required
@@ -20,3 +22,17 @@ def add_photo(request):
         'form': form,
     }
     return render(request, 'photos/add-photo-page.html', context)
+
+
+def details_photo(request, pk):
+    photo = Photo.objects.filter(pk=pk).get()
+
+    context = {
+        'photo': photo,
+        'is_owner': photo.user == request.user,
+    }
+
+    return render(request, 'photos/details-photo-page.html', context)
+
+def edit_photo(request, pk):
+    pass
